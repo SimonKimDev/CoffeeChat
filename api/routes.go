87ba7c68900b225c0c5bdde405556d9259e01fe4
SimonKimDev <smkim216@gmail.com>
2025-04-30 23:57:37 -0700

    add router

---
+++ api/routes.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func RegisterRoutes(server *http.ServeMux) {
+	server.HandleFunc("GET /hello", helloHandler)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
