a3621aa3709f9ef8cded7d123ea8373dd5267e25
SimonKimDev <smkim216@gmail.com>
2025-05-24 00:17:14 -0700

    modify main to listen to port set by config, add get post by category endpoint

--- internal/application/postService.go
+++ internal/application/postService.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/SimonKimDev/CoffeeChat/internal/domain/blog"
 	"github.com/SimonKimDev/CoffeeChat/internal/infra/db"
@@ -10,7 +11,8 @@ import (
 type BlogPoster interface {
 	CreatePost(context.Context, *blog.Post) error
 	GetAllPost(context.Context) (*[]blog.Post, error)
-	GetPostById(context.Context, int64) (*blog.Post, error)
+	GetPostByID(context.Context, int64) (*blog.Post, error)
+	GetPostByCategoryID(context.Context, int64) (*[]blog.Post, error)
 	UpdatePost(context.Context, *blog.Post) error
 	DeletePostByID(context.Context, int64) error
 }
@@ -25,19 +27,28 @@ func NewPostService() BlogPoster {
 func (*postService) CreatePost(ctx context.Context, post *blog.Post) error {
 	const query = `
         INSERT INTO blog.post(author_id, category_id, title, slug, summary, body_markdown, date_published, date_updated)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, NOW())
+        VALUES ($1, $2, $3, $4, $5, $6, NOW(), NOW())
         RETURNING post_id;`
+
+	category := sql.NullInt64{Valid: post.CategoryID != nil}
+	if post.CategoryID != nil {
+		category.Int64 = *post.CategoryID
+	}
+
+	summary := sql.NullString{Valid: post.Summary != nil}
+	if post.Summary != nil {
+		summary.String = *post.Summary
+	}
 
 	// common practice is to use Exec, but postgres doesn't allow LastInsertedId
 	err := db.DB.QueryRowContext(ctx, query,
-		post.AuthorId,
-		post.CategoryId,
+		post.AuthorID,
+		category,
 		post.Title,
 		post.Slug,
-		post.Summary,
+		summary,
 		post.BodyMarkdown,
-		post.DatePublished,
-	).Scan(&post.PostId)
+	).Scan(&post.PostID)
 
 	return err
 }
@@ -57,7 +68,7 @@ func (*postService) GetAllPost(ctx context.Context) (*[]blog.Post, error) {
 
 	for rows.Next() {
 		var post blog.Post
-		err := rows.Scan(&post.PostId, &post.AuthorId, &post.CategoryId, &post.Title, &post.Slug, &post.Summary, &post.BodyMarkdown, &post.DatePublished, &post.DateUpdated)
+		err := rows.Scan(&post.PostID, &post.AuthorID, &post.CategoryID, &post.Title, &post.Slug, &post.Summary, &post.BodyMarkdown, &post.DatePublished, &post.DateUpdated)
 
 		if err != nil {
 			return nil, err
@@ -69,19 +80,46 @@ func (*postService) GetAllPost(ctx context.Context) (*[]blog.Post, error) {
 	return &posts, nil
 }
 
-func (*postService) GetPostById(ctx context.Context, postId int64) (*blog.Post, error) {
+func (*postService) GetPostByID(ctx context.Context, postID int64) (*blog.Post, error) {
 	const query = `SELECT * FROM blog.post WHERE post_id = $1`
 
-	row := db.DB.QueryRowContext(ctx, query, postId)
+	row := db.DB.QueryRowContext(ctx, query, postID)
 
 	var post blog.Post
-	err := row.Scan(&post.PostId, &post.AuthorId, &post.CategoryId, &post.Title, &post.Slug, &post.Summary, &post.BodyMarkdown, &post.DatePublished, &post.DateUpdated)
+	err := row.Scan(&post.PostID, &post.AuthorID, &post.CategoryID, &post.Title, &post.Slug, &post.Summary, &post.BodyMarkdown, &post.DatePublished, &post.DateUpdated)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return &post, nil
+}
+
+// TODO: This needs to be paginated as well
+func (*postService) GetPostByCategoryID(ctx context.Context, categoryID int64) (*[]blog.Post, error) {
+	const query = `SELECT * FROM blog.post WHERE category_id = $1`
+
+	rows, err := db.DB.QueryContext(ctx, query, categoryID)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var posts []blog.Post
+
+	for rows.Next() {
+		var post blog.Post
+		err := rows.Scan(&post.PostID, &post.AuthorID, &post.CategoryID, &post.Title, &post.Slug, &post.Summary, &post.BodyMarkdown, &post.DatePublished, &post.DateUpdated)
+
+		if err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, post)
+	}
+
+	return &posts, nil
 }
 
 func (*postService) UpdatePost(ctx context.Context, post *blog.Post) error {
@@ -95,12 +133,22 @@ func (*postService) UpdatePost(ctx context.Context, post *blog.Post) error {
             date_updated = NOW()
         WHERE post_id = $5`
 
+	category := sql.NullInt64{Valid: post.CategoryID != nil}
+	if post.CategoryID != nil {
+		category.Int64 = *post.CategoryID
+	}
+
+	summary := sql.NullString{Valid: post.Summary != nil}
+	if post.Summary != nil {
+		summary.String = *post.Summary
+	}
+
 	_, err := db.DB.ExecContext(ctx, query,
-		post.CategoryId,
+		category,
 		post.Title,
-		post.Summary,
+		summary,
 		post.BodyMarkdown,
-		post.PostId)
+		post.PostID)
 
 	return err
 }
